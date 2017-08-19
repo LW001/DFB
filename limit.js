54d3d5176ea2b3ad120894d2f29f6cc1706513db
LW001 <lw001business@gmail.com>
2017-08-20 00:42:08 +0200

    :ok_hand:

--- limit.js
+++ limit.js
@@ -4,17 +4,17 @@ module.exports = {
     submit: 60000,
     info: 100000,
     url: 30000,
-    vote: 0,
-    help: 0,
-    stats: 0
+    vote: 1000,
+    help: 1000,
+    stats: 1000
   },
   mod: {
     comment: 20000,
     submit: 40000,
     info: 600000,
     url: 60000,
-    vote: 0,
-    help: 0,
-    stats: 0
+    vote: 1000,
+    help: 1000,
+    stats: 1000
   }
 }
